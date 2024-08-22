e38f4b674fad71fef11a75f465c84c9a06e1ceec
prathyusha <prathyusha3294>
2024-08-22 09:32:22 +0530

    updated employeeviewset

--- EmployeeManagement/organization/urls.py
+++ EmployeeManagement/organization/urls.py
@@ -11,10 +11,6 @@ company_router.register('master/employee', EmployeeViewSet, basename='company')
 urlpatterns = [
     path('', views.getEmployeeList, name='getEmployeeList'), 
     path('',include(company_router.urls)),
-    # path('employees/', views.fetchEmployeeList, name='fetchEmployeeList'),  # API to get employee data
-    # path('create/', views.createEmployee, name='createEmployee'),
-    # path('<int:employeeId>/edit/', views.updateEmployee, name='updateEmployee'),
-    # path('<int:employeeId>/delete/', views.deleteEmployee, name='deleteEmployee'),
 ]
 
 
